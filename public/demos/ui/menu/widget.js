43d8bfe03527513e5559119ac8e739f2e6dfbbbf
wangbow <234211172@qq.com>
2016-12-01 15:42:09 +0800

    修改下拉菜单涉及到的bug等

--- public/demos/ui/menu/widget.js
+++ public/demos/ui/menu/widget.js
@@ -29,44 +29,49 @@
             $ul.hide();
         }
     });
+
     $('.u-button-level-hover').hover(function () {
-        var left=$(this).parent().get(0).offsetLeft+$(this).parent().get(0).clientWidth;
+        var left=/*$(this).parent().get(0).offsetLeft+*/$(this).parent().get(0).clientWidth+1;
         var top=$(this).get(0).offsetTop;
         var $ul=$(this).children('ul');
         $ul.css('left',left);
-        $ul.css('top',top);
+        $ul.css('top',0);
         $ul.show();
     },function () {
-        var flag=true;
-        var $ul=$(this).children('ul');
-        $ul.hover(function () {
-            flag=false;
-            $ul.show();
-        },function () {
-            $ul.hide();
-            $ul.parent().parent().hide();
-        });
-        if(flag){
-            $ul.hide();
-        }
+        var self=this;
+       setTimeout(function(){
+           var flag=true;
+           var $ul=$(self).children('ul');
+           $ul.hover(function () {
+               flag=false;
+               $ul.show();
+           },function () {
+               $ul.hide();
+               $ul.parent().parent().hide();
+           });
+           if(flag){
+               $ul.hide();
+           }
+       },300)
     });
     /*向上对齐的二级菜单*/
     $('.u-button-level-dropup').on('click',function () {
         var $ul=$(this).next();
         if($ul.is(':hidden')){
+         /*   $ul.css("top","-84px");*/
             $ul.show();
         }else{
             $ul.hide();
         }
     });
     $('.u-button-level-hover-dropup').hover(function () {
-        var left=$(this).parent().get(0).offsetLeft+$(this).parent().get(0).clientWidth;
+        var left=/*$(this).parent().get(0).offsetLeft+*/$(this).parent().get(0).clientWidth+1;
         var $ul=$(this).children('ul');
         $ul.css('left',left);
         $ul.css('top',0);
         $ul.show();
     },function () {
-        var flag=true;
+     var flag=true;
         var $ul=$(this).children('ul');
         $ul.hover(function () {
             flag=false;
