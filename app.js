11901e0113cdb9ea26eef452f5abf2fe06bd5f1b
huyueb <1062887235@qq.com>
2017-05-22 11:03:13 +0800

    由于改动端口号会导致其他测试环境和正式环境的配置都得改掉端口号，所以修改回原来的端口号

--- app.js
+++ app.js
@@ -35,6 +35,6 @@ app.use(function *(next){
 // app.use(serve(path.join(__dirname, './public')));
 app.use(serve(path.join(__dirname, '')));
 
-app.listen( 8070 );
+app.listen( 8090 );
 
-console.log('server started at http://localhost:8070');
+console.log('server started at http://localhost:8090');
